969ea588ad973142f4e7bbcaf950164b459221cf
Tanzeel-khan <ktanzeel184@gmail.com>
2020-06-27 01:38:07 +0500

    practice js operators and assignments,objects

---
+++ app.js
@@ -0,0 +1,9 @@
+//operators
+var x =5;
+var y = x ** 2;
+alert("square is"+y);
+var z= Math.pow(x,2);
+alert("square is"+z);
+var carname1="bmw";
+var carname2="civic reborn";
+document.getElementById("demo").innerHTML= carname1 + "<br>"+carname2;

b65e9c47a71e94d3fbbad9d23ca57151abb0e81b
Tanzeel-khan <ktanzeel184@gmail.com>
2020-06-27 02:03:39 +0500

    objects in js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //operators
+
 var x =5;
 var y = x ** 2;
 alert("square is"+y);
@@ -7,3 +8,14 @@ alert("square is"+z);
 var carname1="bmw";
 var carname2="civic reborn";
 document.getElementById("demo").innerHTML= carname1 + "<br>"+carname2;
+var answer1= "alright";
+var answer2 = "its 'alright'";
+var answer3 = 'its "ok" tanzeel';
+
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age     : 50,
+    eyeColor  : "blue"
+  };
+document.getElementById("demo2").innerHTML= person.firstName + "is" + person.age + "years old."; 
